5e5facfef62d9f08e7fe252c0e42f4cbfb1cb112
Corentin MAS <corentinmas@yahoo.com>
2022-05-16 18:16:51 +0200

    [main] Fixing linter

--- apps/frontend/App.tsx
+++ apps/frontend/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+// import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import HomeScreen from "./src/screens/HomeScreen";
-import LoginScreen from "./src/screens/LoginScreen";
-import RegisterScreen from "./src/screens/RegisterScreen";
-import Dashboard from "./src/screens/Dashboard";
-import ConnectAccounts from "./src/screens/ConnectAccounts";
+import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import Dashboard from './src/screens/Dashboard';
+// import ConnectAccounts from './src/screens/ConnectAccounts';
 
 // const App = () => {
 //   return (
@@ -17,18 +17,17 @@ import ConnectAccounts from "./src/screens/ConnectAccounts";
 // };
 
 const Router = createStackNavigator(
-  {
-    HomeScreen,
-    LoginScreen,
-    RegisterScreen,
-    Dashboard,
-    ConnectAccounts,
-
-  },
-  {
-    initialRouteName: "HomeScreen",
-    headerMode: "none"
-  }
+    {
+        HomeScreen,
+        LoginScreen,
+        RegisterScreen,
+        Dashboard,
+        // ConnectAccounts,
+    },
+    {
+        initialRouteName: 'HomeScreen',
+        headerMode: 'none',
+    },
 );
 
 // export default App;
