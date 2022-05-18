24866f38212dca150de1b18c7ebc19c992ac9fc3
Corentin MAS <corentinmas@yahoo.com>
2022-05-18 18:16:55 +0200

    [feature/openapi] Added openapi request + response validator

---
+++ apps/api/configuration.ts
@@ -0,0 +1,3 @@
+export default {
+    OPENAPI_SPEC_DEFINITION: process.env.OPENAPI_SPEC_DEFINITION ?? './entities/openapi.index.json',
+};
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,6 +6,11 @@ import session from 'express-session';
 import { User } from './types';
 import cookieParser from 'cookie-parser';
 
+import configuration from '../configuration';
+import * as OpenApiValidator from 'express-openapi-validator';
+
+const openApiDocument = require(configuration.OPENAPI_SPEC_DEFINITION);
+
 const sessionConfig = {
     user: {},
     secret: process.env.COOKIE_SECRET || 'secret',
@@ -23,23 +28,44 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+    OpenApiValidator.middleware({
+        apiSpec: openApiDocument,
+        validateRequests: {
+            removeAdditional: 'all',
+            allowUnknownQueryParameters: false,
+            coerceTypes: false,
+        },
+        ignoreUndocumented: true,
+        validateResponses: {
+            removeAdditional: 'all',
+        },
+        validateFormats: 'full',
+    }),
+);
+
 app.use(session(sessionConfig));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/v1/', routes);
-
-// login all requests
 app.use((req: Request, res: Response, next: NextFunction) => {
     console.log(`${req.method} ${req.path}`);
     next();
 });
 
+app.use('/v1/', routes);
+
+// login all requests
+
+app.use((err: any, req: Request, res: Response, $next: NextFunction) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        status: 'failed',
+        errors: err.errors,
+    });
+});
+
 app.listen(8080, () => {
     console.log('Listening on port 8080');
 });
-/*
-MongoDB.getById('cars', "_id").then(console.log); */
-
-//toto et cars as collection
