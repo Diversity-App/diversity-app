7b79e8d426bc9625c2a65e16b5aef89f4cf2b73b
Corentin MAS <corentinmas@yahoo.com>
2022-06-14 13:34:32 +0200

    [feature/api/test-extended] Added SSO Tests

--- apps/api/tests/auth/auth.spec.ts
+++ apps/api/tests/auth/auth.spec.ts
@@ -6,10 +6,20 @@ import { LoginRequest, RegisterRequest } from '../../../../shared/services';
 import app from '../../src/app';
 
 import prisma from '../../src/tools/prisma';
+import { verify } from 'jsonwebtoken';
+import GoogleService from '../../src/entities/auth/sso/google/google.service';
+import SsoTool from '../../src/tools/sso.tool';
+
+jest.mock('jsonwebtoken');
+const user = {
+    id: 1,
+};
 
 jest.mock('../../src/tools/auth.tools');
+jest.mock('../../src/entities/auth/sso/google/google.service');
+jest.mock('../../src/tools/sso.tool');
 
-describe('Auth', () => {
+describe('Auth Basic', () => {
     const expectedUser = {
         id: 1,
         uuid: 'any-uuid',
@@ -52,6 +62,118 @@ describe('Auth', () => {
 
             const response = await supertest(app).post('/v1/auth/login').send(loginReq);
             expect(response.status).toBe(200);
+        });
+    });
+});
+
+describe('Auth SSO', () => {
+    describe('Google', () => {
+        it('should successfully redirect', async () => {
+            const response = await supertest(app).get('/v1/auth/sso/google/login');
+            // should redirect to consent screen
+            expect(response.status).toBe(302);
+        });
+
+        it('should successfully link', async () => {
+            // Mock JWT validation
+            (verify as jest.MockedFunction<typeof verify>).mockReturnValueOnce(user as any);
+
+            const agent = supertest.agent(app);
+            agent.auth('123', { type: 'bearer' });
+
+            // GoogleService
+            (GoogleService.fetchToken as jest.MockedFunction<typeof GoogleService.fetchToken>).mockResolvedValueOnce({
+                access_token: 'access_token',
+                refresh_token: 'refresh_token',
+                expires_in: '3600',
+            });
+
+            (GoogleService.fetchUser as jest.MockedFunction<typeof GoogleService.fetchUser>).mockResolvedValueOnce({
+                id: 'number',
+                uuid: 'string',
+                name: 'string',
+                username: 'string',
+            });
+
+            // SsoTool
+            (SsoTool.syncUserToken as jest.MockedFunction<typeof SsoTool.syncUserToken>).mockResolvedValueOnce();
+
+            const response = await agent
+                .get('/v1/auth/sso/google/callback?code=' + 'templated-value')
+                .set('Authorization', 'Bearer 123');
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual({
+                message: 'Authentication successful',
+                status: 'success',
+                token: response.body.token,
+            });
+        });
+
+        it('should fail to link without credentials', async () => {
+            // Mock JWT validation
+            // (verify as jest.MockedFunction<typeof verify>).mockReturnValueOnce(user as any);
+
+            const agent = supertest.agent(app);
+            agent.auth('123', { type: 'bearer' });
+
+            const response = await agent
+                .get('/v1/auth/sso/google/callback?code=' + 'templated-value')
+                .set('Authorization', 'Bearer 123');
+
+            expect(response.status).toBe(401);
+            expect(response.body).toHaveProperty('status');
+            expect(response.body.status).toEqual('error');
+        });
+
+        it('should fail to link with third party error', async () => {
+            // Mock JWT validation
+            // (verify as jest.MockedFunction<typeof verify>).mockReturnValueOnce(user as any);
+
+            const agent = supertest.agent(app);
+            agent.auth('123', { type: 'bearer' });
+
+            const response = await agent
+                .get('/v1/auth/sso/google/callback?code=' + 'templated-value')
+                .set('Authorization', 'Bearer 123');
+
+            expect(response.status).toBe(500);
+            expect(response.body).toHaveProperty('status');
+            expect(response.body.status).toEqual('error');
+        });
+
+        it('should fail to link caused by duplicate user', async () => {
+            (verify as jest.MockedFunction<typeof verify>).mockReturnValueOnce(user as any);
+
+            const agent = supertest.agent(app);
+            agent.auth('123', { type: 'bearer' });
+
+            // GoogleService
+            (GoogleService.fetchToken as jest.MockedFunction<typeof GoogleService.fetchToken>).mockResolvedValueOnce({
+                access_token: 'access_token',
+                refresh_token: 'refresh_token',
+                expires_in: '3600',
+            });
+
+            (GoogleService.fetchUser as jest.MockedFunction<typeof GoogleService.fetchUser>).mockResolvedValueOnce({
+                id: 'number',
+                uuid: 'string',
+                name: 'string',
+                username: 'string',
+            });
+
+            // SsoTool
+            (SsoTool.syncUserToken as jest.MockedFunction<typeof SsoTool.syncUserToken>).mockImplementationOnce(() => {
+                throw new Error('Duplicate user');
+            });
+
+            const response = await agent
+                .get('/v1/auth/sso/google/callback?code=' + 'templated-value')
+                .set('Authorization', 'Bearer 123');
+
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('status');
+            expect(response.body.status).toEqual('error');
         });
     });
 });
