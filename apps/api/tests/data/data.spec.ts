9d4c72f88e9a40ec6fe54a945b497c3bad5af98a
Carlos <carlos.frangeard@epitech.eu>
2022-06-14 11:36:08 +0200

    [features/twitter-sso][ADD] not tested routes

--- apps/api/tests/data/data.spec.ts
+++ apps/api/tests/data/data.spec.ts
@@ -20,6 +20,8 @@ const user = {
 //     tagsRatio: [],
 // };
 describe('Data', () => {
+    const jwt =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU1MTQzMjc2LCJleHAiOjE2NTUxODY0NzZ9.xFbq3e0AwRNlULWhtPvOqet2I80qSGrpfI2C1-NDhik';
     describe('get homepage', () => {
         beforeEach(() => {
             (verify as jest.MockedFunction<typeof verify>).mockReturnValueOnce(user as any);
@@ -31,6 +33,20 @@ describe('Data', () => {
             const response = await agent.get('/v1/data/homePage').set('Authorization', 'Bearer 123');
 
             expect(response.status).toBe(401);
+        });
+    });
+
+    describe('get liked tweets', () => {
+        beforeEach(() => {
+            (verify as jest.MockedFunction<typeof verify>).mockReturnValueOnce(user as any);
+        });
+        afterEach(() => {});
+        it('should successfully return data', async () => {
+            const agent = supertest.agent(app);
+            agent.auth('abc', { type: 'bearer' });
+            const response = await agent.get('/v1/data/twitter/liked').set('Authorization', `Bearer ${jwt}`);
+
+            expect(response.status).toBe(200);
         });
     });
 });
