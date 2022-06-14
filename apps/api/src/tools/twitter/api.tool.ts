0866e2ae8f641095fa401dd3a69c7a4c4d191a10
Carlos <carlos.frangeard@epitech.eu>
2022-06-14 15:51:35 +0200

    [features/twitter-sso][ADD] liked tweet test

--- apps/api/src/tools/twitter/api.tool.ts
+++ apps/api/src/tools/twitter/api.tool.ts
@@ -3,7 +3,6 @@ import { ILiked } from './types';
 
 export default class TwitterApiWrapper {
     static async getUserLikedTweets(userID: string, token: string): Promise<ILiked> {
-        console.log(userID, token);
         const url = `https://api.twitter.com/2/users/${userID}/liked_tweets`;
         const response = await axios(url, {
             headers: {
@@ -15,23 +14,3 @@ export default class TwitterApiWrapper {
         return data;
     }
 }
-
-/*
-   const sqlResponse = await getAccessToken(req, "twitter");
-    // const { userId } = req.query;
-
-    if (!sqlResponse.query) return res.status(500).json({ code: sqlResponse.code, error: sqlResponse.error });
-    // if (!userId) return res.status(500).json({ code: 500, error: "Invalid UserID" });
-
-    try {
-      const url = `https://api.twitter.com/2/users/${sqlResponse.account.id}/liked_tweets?expansions=author_id`;
-      const response = await axios(url, {
-        headers: { Authorization: `Bearer ${sqlResponse.account.token}` },
-      });
-      res.status(200).json({ code: 200, data: (await response.data.data) ? response.data.data : [] });
-    } catch (e) {
-      const err = e as AxiosError;
-      if (err.response) return res.status(500).json({ code: 500, error: err.response.data.detail });
-      res.status(500).json({ code: 500, error: "[TWITTER][/userLikedTweets] failed" });
-    }
-    */
--- apps/api/tests/data/data.spec.ts
+++ apps/api/tests/data/data.spec.ts
@@ -8,20 +8,10 @@ import { verify } from 'jsonwebtoken';
 jest.mock('jsonwebtoken');
 
 const user = {
-    id: 1,
+    id: 6,
 };
-//
-// const data: StatSummary = {
-//     items: [],
-//     categories: [],
-//     total: 0,
-//     categoriesRatio: [],
-//     tags: [],
-//     tagsRatio: [],
-// };
+
 describe('Data', () => {
-    const jwt =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU1MTQzMjc2LCJleHAiOjE2NTUxODY0NzZ9.xFbq3e0AwRNlULWhtPvOqet2I80qSGrpfI2C1-NDhik';
     describe('get homepage', () => {
         beforeEach(() => {
             (verify as jest.MockedFunction<typeof verify>).mockReturnValueOnce(user as any);
@@ -37,15 +27,11 @@ describe('Data', () => {
     });
 
     describe('get liked tweets', () => {
-        beforeEach(() => {
-            (verify as jest.MockedFunction<typeof verify>).mockReturnValueOnce(user as any);
-        });
-        afterEach(() => {});
         it('should successfully return data', async () => {
+            (verify as jest.MockedFunction<typeof verify>).mockReturnValue(user as any);
             const agent = supertest.agent(app);
-            agent.auth('abc', { type: 'bearer' });
-            const response = await agent.get('/v1/data/twitter/liked').set('Authorization', `Bearer ${jwt}`);
-
+            agent.auth('123', { type: 'bearer' });
+            const response = await agent.get('/v1/data/twitter/liked').set('Authorization', `Bearer 123`);
             expect(response.status).toBe(200);
         });
     });
